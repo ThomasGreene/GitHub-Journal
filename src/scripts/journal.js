e642444af0ccf16a77ac4fbc048500909ae45db0
Mark Greene <mrkgreene7@gmail.com>
2019-06-05 17:35:29 -0500

    Daily Journal Entries 1 and 2

---
+++ src/scripts/journal.js
@@ -0,0 +1,30 @@
+const journalEntries = [];
+
+const journalEntry1 = {
+    date: "05/29/2019",
+    concept: "functions",
+    entry: "We started the basics of functions",
+    mood: "happy"
+}
+journalEntries.push(journalEntry1)
+console.log(journalEntries)
+
+
+const journalEntry2 = {
+    date: "06/04/2019",
+    concept: "objects representing things",
+    entry: "By far the most difficult thing I've had to learn so far.  Need to get better with for loops and if statements",
+    mood: "frustrated"
+}
+journalEntries.push(journalEntry2)
+console.log(journalEntries)
+
+
+const journalEntry3 = {
+    date: "06/05/2019",
+    concept: "functions",
+    entry: "Started working with JSON and Postman. Need to review previous chapters up to this point",
+    mood: "happy"
+}
+journalEntries.push(journalEntry3)
+console.log(journalEntries)
